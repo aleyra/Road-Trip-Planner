7796a7ccd80f5ca094906dafd62494c9e167cbb3
Lucille Burnet <lburnet@z3r7p5.42lyon.fr>
2024-05-30 20:01:05 +0200

    MapContainer start

---
+++ front/src/containers/MapContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//css
+
+
+function MapContainer() {
+    return (
+        <React.Fragment>
+                    <p>map TODO</p>
+        </React.Fragment>
+    );
+}
